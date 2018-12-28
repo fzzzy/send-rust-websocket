4ac567c9b2475d4c68078e6b82e51d95f6b4e1d4
Donovan Preston <donovanpreston@gmail.com>
2018-12-27 19:20:17 -0500

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ use std::thread;
 
 use websocket::OwnedMessage;
 use websocket::sender::Writer;
+use websocket::server::upgrade::WsUpgrade;
 use websocket::sync::Server;
+use websocket::sync::server::upgrade::Buffer;
 
 fn close(mut sender: Writer<TcpStream>, ip: SocketAddr) {
     let message = OwnedMessage::Close(None);
@@ -19,74 +21,82 @@ fn close(mut sender: Writer<TcpStream>, ip: SocketAddr) {
     println!("DISCONNECTED: {}", ip);
 }
 
-fn main() {
+fn accepted(request: WsUpgrade<TcpStream, Option<Buffer>>) {
+    let rand = SystemRandom::new();
+    let client = request.accept().unwrap();
+    let ip = client.peer_addr().unwrap();
+    let (mut receiver, mut sender) = client.split().unwrap();
+    let mut first_message = true;
+
+    println!("CONNECTED: {}", ip);
+
+    for message in receiver.incoming_messages() {
+        let message = message.unwrap();
+
+        match message {
+            OwnedMessage::Close(_) => {
+                return close(sender, ip);
+            }
+            OwnedMessage::Ping(ping) => {
+                let message = OwnedMessage::Pong(ping);
+                sender.send_message(&message).unwrap();
+            }
+            OwnedMessage::Text(_msg) => {
+                if first_message {
+                    first_message = false;
+                    //println!("First message {}", msg);
+                    // TODO Randomly generate values here
+                    let mut id: [u8; 5] = [0; 5];
+                    let mut owner: [u8; 10] = [0; 10];
+                    rand.fill(&mut id).unwrap();
+                    rand.fill(&mut owner).unwrap();
+
+                    let json_data = json!({
+                        "url": format!("http://localhost:8080/download/{:02x}/", id.iter().format("")),
+                        "ownerToken": format!("{:02x}", owner.iter().format("")),
+                        "id": format!("{:02x}", id.iter().format(""))
+                    });
+                    let reply = OwnedMessage::Text(json_data.to_string());
+                    sender.send_message(&reply).unwrap();
+                } else {
+                    println!("{}: Got non-first message; closing socket", ip);
+                    return close(sender, ip);
+                }
+            }
+            OwnedMessage::Binary(bin) => {
+                if first_message {
+                    println!("{}: Got binary message before json metadata; closing socket", ip);
+                    return close(sender, ip);
+                }
+
+                if bin.len() == 1 &&  bin[0] == 0 {
+                    let json_data = json!({
+                        "ok": true
+                    });
+                    let reply = OwnedMessage::Text(json_data.to_string());
+                    sender.send_message(&reply).unwrap();
+                }
+            }
+            OwnedMessage::Pong(_) => {
+                println!("{}: Got pong message, but we should never get a pong; closing socket", ip);
+                return close(sender, ip);
+            }
+        }
+    }
+}
+
+fn websocket() {
     let server = Server::bind("127.0.0.1:12223").unwrap();
 
     for request in server.filter_map(Result::ok) {
         thread::spawn(move || {
-            let rand = SystemRandom::new();
-            let client = request.accept().unwrap();
-            let ip = client.peer_addr().unwrap();
-            let (mut receiver, mut sender) = client.split().unwrap();
-            let mut first_message = true;
-
-            println!("CONNECTED: {}", ip);
-
-            for message in receiver.incoming_messages() {
-                let message = message.unwrap();
-
-                match message {
-                    OwnedMessage::Close(_) => {
-                        return close(sender, ip);
-                    }
-                    OwnedMessage::Ping(ping) => {
-                        let message = OwnedMessage::Pong(ping);
-                        sender.send_message(&message).unwrap();
-                    }
-                    OwnedMessage::Text(_msg) => {
-                        if first_message {
-                            first_message = false;
-                            //println!("First message {}", msg);
-                            // TODO Randomly generate values here
-                            let mut id: [u8; 5] = [0; 5];
-                            let mut owner: [u8; 10] = [0; 10];
-                            rand.fill(&mut id).unwrap();
-                            rand.fill(&mut owner).unwrap();
-
-                            let json_data = json!({
-                                "url": format!("http://localhost:8080/download/{:02x}/", id.iter().format("")),
-                                "ownerToken": format!("{:02x}", owner.iter().format("")),
-                                "id": format!("{:02x}", id.iter().format(""))
-                            });
-                            let reply = OwnedMessage::Text(json_data.to_string());
-                            sender.send_message(&reply).unwrap();
-                        } else {
-                            println!("{}: Got non-first message; closing socket", ip);
-                            return close(sender, ip);
-                        }
-                    }
-                    OwnedMessage::Binary(bin) => {
-                        if first_message {
-                            println!("{}: Got binary message before json metadata; closing socket", ip);
-                            return close(sender, ip);
-                        }
-
-                        if bin.len() == 1 &&  bin[0] == 0 {
-                            let json_data = json!({
-                                "ok": true
-                            });
-                            let reply = OwnedMessage::Text(json_data.to_string());
-                            sender.send_message(&reply).unwrap();
-                        }
-                    }
-                    OwnedMessage::Pong(_) => {
-                        println!("{}: Got pong message, but we should never get a pong; closing socket", ip);
-                        return close(sender, ip);
-                    }
-                }
-            }
+            accepted(request);
         });
     }
+}
+
+fn main() {
+    websocket();
 }
 
 
