fbc9f51a8f3526188cedfae8339d7219865df945
Donovan Preston <donovanpreston@gmail.com>
2019-01-04 17:51:43 -0500

    Save the streamed files to disk and serve the encrypted files from disk at the /download/ url (useless because it's not decryptable, but this is an intermediate step)

--- src/sendapi.rs
+++ src/sendapi.rs
@@ -1,5 +1,8 @@
 
-use actix_web::{http, server, App, Path, Responder, HttpRequest, HttpResponse, Result};
+use std::io::prelude::*;
+use std::fs::File;
+
+use actix_web::{http, server, App, Path, HttpRequest, HttpResponse, Result};
 
 const HTML: &'static str = include_str!("websockets.html");
 
@@ -9,17 +12,22 @@ fn home(_req: &HttpRequest) -> Result<HttpResponse> {
         .body(HTML))
 }
 
-fn download(id: Path<String>) -> impl Responder {
-    let out = format!("Hello {}!\n", id);
-    println!("{}", out);
-    out
+fn download(id: Path<String>) -> Result<HttpResponse> {
+    let mut file = File::open(format!("{}.send", id)).unwrap();
+    let mut contents = vec![];
+    println!("Download {}", id);
+    file.read_to_end(&mut contents).unwrap();
+    // response
+    Ok(HttpResponse::build(http::StatusCode::OK)
+        .content_type("application/octet-stream")
+        .body(contents))
 }
 
 pub fn api() {
     server::new(
         || App::new()
             .resource("/", |r| r.method(http::Method::GET).f(home))
-            .route("/download/{id}", http::Method::GET, download))
-        .bind("127.0.0.1:8088").unwrap()
+            .route("/download/{id}/", http::Method::GET, download))
+        .bind("127.0.0.1:12224").unwrap()
         .run();
 }
--- src/sendws.rs
+++ src/sendws.rs
@@ -3,6 +3,8 @@
 use itertools::Itertools;
 use ring::rand::{SystemRandom, SecureRandom};
 
+use std::io::prelude::*;
+use std::fs::File;
 use std::net::{SocketAddr, TcpStream};
 use std::thread;
 
@@ -25,6 +27,7 @@ pub fn accepted(request: WsUpgrade<TcpStream, Option<Buffer>>) {
     let ip = client.peer_addr().unwrap();
     let (mut receiver, mut sender) = client.split().unwrap();
     let mut first_message = true;
+    let mut file = File::open("/dev/null").unwrap();
 
     println!("CONNECTED: {}", ip);
 
@@ -43,18 +46,19 @@ pub fn accepted(request: WsUpgrade<TcpStream, Option<Buffer>>) {
                 if first_message {
                     first_message = false;
                     println!("First message {}", msg);
-                    let mut id: [u8; 5] = [0; 5];
                     let mut owner: [u8; 10] = [0; 10];
+                    let mut id: [u8; 5] = [0; 5];
                     rand.fill(&mut id).unwrap();
                     rand.fill(&mut owner).unwrap();
 
                     let json_data = json!({
-                        "url": format!("http://localhost:12223/download/{:02x}/", id.iter().format("")),
+                        "url": format!("http://localhost:12224/download/{:02x}/", id.iter().format("")),
                         "ownerToken": format!("{:02x}", owner.iter().format("")),
                         "id": format!("{:02x}", id.iter().format(""))
                     });
                     let reply = OwnedMessage::Text(json_data.to_string());
                     sender.send_message(&reply).unwrap();
+                    file = File::create(format!("{:02x}.send", id.iter().format(""))).unwrap();
                 } else {
                     println!("{}: Got non-first message; closing socket", ip);
                     return close(sender, ip);
@@ -72,6 +76,8 @@ pub fn accepted(request: WsUpgrade<TcpStream, Option<Buffer>>) {
                     });
                     let reply = OwnedMessage::Text(json_data.to_string());
                     sender.send_message(&reply).unwrap();
+                } else {
+                    file.write_all(&bin).unwrap();
                 }
             }
             OwnedMessage::Pong(_) => {
